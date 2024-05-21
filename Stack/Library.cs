0afdb3c2d062f0d0ed9395eb0d7eeecf3c65460f
Alkin Ivan <alkinivan_2003@mail.ru>
2024-05-21 22:05:57 +0400

    Добавьте файлы проекта.

---
+++ Stack/Library.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Library
+{
+    private Book[] books;
+    private int top;
+    private int capacity;
+
+    public Library(int size)
+    {
+        capacity = size;
+        books = new Book[capacity];
+        top = -1;
+    }
+
+    public void AddBook(Book book)
+    {
+        if (top == capacity - 1)
+        {
+            Console.WriteLine("Библиотека заполнена. Невозможно добавить больше книг.");
+            return;
+        }
+        books[++top] = book;
+        Console.WriteLine($"Добавлена книга: {book}");
+        Console.WriteLine();
+    }
+
+    public Book BorrowBook()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Нет доступных книг в библиотеке.");
+            return null;
+        }
+
+        Book borrowedBook = books[top];
+        books[top--] = null;
+        Console.WriteLine($"Взята книга: {borrowedBook}");
+        Console.WriteLine();
+        return borrowedBook;
+    }
+
+    public void DisplayBooks()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Библиотека пуста.");
+            return;
+        }
+
+        Console.WriteLine("Доступные книги в библиотеке:");
+        for (int i = top; i >= 0; i--)
+        {
+            Console.WriteLine(books[i]);
+        }
+        Console.WriteLine();
+    }
+
+    public Book FindBookByTitle(string title)
+    {
+        for (int i = top; i >= 0; i--)
+        {
+            if (books[i].Title.Equals(title, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"Найдена книга по названию \"{title}\": {books[i]}\n");
+                return books[i];
+            }
+        }
+
+        Console.WriteLine($"Книга с названием \"{title}\" не найдена в библиотеке.");
+        Console.WriteLine();
+        return null;
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Library library = new Library(5);
+
+        // Добавление книг в библиотеку
+        library.AddBook(new Book("Мёртвые души", "Николай Гоголь", 1835));
+        library.AddBook(new Book("Война и мир", "Лев Толстой", 1863));
+        library.AddBook(new Book("Гарри Поттер и философский камень", "Джоан Роулинг", 1997));
+
+        // Отображение доступных книг
+        library.DisplayBooks();
+
+        // Взятие книги
+        Book borrowedBook = library.BorrowBook();
+
+        // Отображение доступных книг после взятия
+        library.DisplayBooks();
+
+        string BookTitle1 = "Мёртвые души";
+        string BookTitle2 = "Гарри Поттер и философский камень";
+        library.FindBookByTitle(BookTitle2);
+        library.FindBookByTitle(BookTitle1);
+    }
+}
